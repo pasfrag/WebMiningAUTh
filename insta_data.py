eab272381d0d2c6bf95ecca44e259d3842c3246f
Paschalis Frangidis <pascal.frag@gmail.com>
2020-05-15 14:24:03 +0300

    final retrieval and reprocessing

--- insta_data.py
+++ insta_data.py
@@ -1,56 +1,93 @@
 import json
-from secret_keys import insta_string
+import datetime
+from secret_keys import insta_username, insta_password
 from instaloader import Instaloader
-import mongo
+from mongo import MongoHandler
+from langdetect import detect
+from preprocessing_functions import preprocess_text, get_hashtags, get_mentions
 
-loader = Instaloader(
-    download_pictures=False, download_video_thumbnails=False, download_videos=False, compress_json=False, sleep=True
-)
-loader.login(insta_string)
 
-for post in loader.get_hashtag_posts('climatechange'):
+class InstaMiner(object):
 
-    # Keeping only necessary k-v
-    # print(post._node)
-    # new_post = dict()
-    # new_post["_id"] = post._node.pop("id")
-    print(json.dumps(post._node, indent=4, sort_keys=True))
-    # try:
-    #     new_post["caption"] = post._node["edge_media_to_caption"]["edges"][0]["node"]["text"]
-    # except:
-    #     new_post["caption"] = None
-    # try:
-    #     new_post["location"] = post._node["location"]
-    # except:
-    #     new_post["location"] = None
-    # try:
-    #     new_post["shortcode"] = post._node["shortcode"]
-    # except:
-    #     new_post["shortcode"] = None
-    # try:
-    #     new_post["timestamp"] = post._node["taken_at_timestamp"]
-    # except:
-    #     new_post["timestamp"] = None
-    # try:
-    #     new_post["liked_by"] = post._node["edge_liked_by"]["count"]
-    # except:
-    #     new_post["liked_by"] = None
-    # try:
-    #     new_post["user_id"] = post._node["owner"]["id"]
-    # except:
-    #     new_post["user_id"] = None
-    # try:
-    #     new_post["username"] = post._node["owner"]["username"]
-    # except:
-    #     new_post["username"] = None
-    # try:
-    #     new_post["is_verified"] = post._node["owner"]["is_verified"]
-    # except:
-    #     new_post["is_verified"] = None
-    # try:
-    #     new_post["is_private"] = post._node["owner"]["is_private"]
-    # except:
-    #     new_post["is_private"] = None
+    loader = None
+    connection = None
 
-    # mongo.create_mongo_collection("instagram", new_post)
-    # print("Hi")
+    def __init__(self):
+        loader = Instaloader(
+            download_pictures=False, download_video_thumbnails=False, download_videos=False, compress_json=False,
+            sleep=True
+        )
+        # loader.login(insta_username, insta_password)
+        self.connection = MongoHandler()
+
+    def get_new_posts(self):
+
+        for post in self.loader.get_hashtag_posts('climatechange'):
+            # Keeping only necessary k-v
+            # print(post._node)
+            new_post = dict()
+            new_post["_id"] = post._node.pop("id")
+            print(json.dumps(post._node, indent=4, sort_keys=True))
+            try:
+                new_post["caption"] = post._node["edge_media_to_caption"]["edges"][0]["node"]["text"]
+            except:
+                new_post["caption"] = None
+            try:
+                new_post["location"] = post._node["location"]
+            except:
+                new_post["location"] = None
+            try:
+                new_post["shortcode"] = post._node["shortcode"]
+            except:
+                new_post["shortcode"] = None
+            try:
+                new_post["timestamp"] = post._node["taken_at_timestamp"]
+            except:
+                new_post["timestamp"] = None
+            try:
+                new_post["liked_by"] = post._node["edge_liked_by"]["count"]
+            except:
+                new_post["liked_by"] = None
+            try:
+                new_post["user_id"] = post._node["owner"]["id"]
+            except:
+                new_post["user_id"] = None
+            try:
+                new_post["username"] = post._node["owner"]["username"]
+            except:
+                new_post["username"] = None
+            try:
+                new_post["is_verified"] = post._node["owner"]["is_verified"]
+            except:
+                new_post["is_verified"] = None
+            try:
+                new_post["is_private"] = post._node["owner"]["is_private"]
+            except:
+                new_post["is_private"] = None
+
+            self.connection.store_to_collection(new_post, "instagram")
+
+    def preprocess_posts(self):
+        posts = self.connection.retrieve_from_collection("instagram")
+
+        count = 0
+        for post in posts:
+            if post["caption"]:
+                try:
+                    if not len(post["caption"].split()) < 5 and detect(post["caption"]) == 'en':
+                        new_post = dict()
+                        new_post["_id"] = int(post["_id"])
+                        new_post["hashtags"] = get_hashtags(post["caption"])
+                        new_post["mentions"] = get_mentions(post["caption"])
+                        new_post["caption"] = preprocess_text(post["caption"])
+                        new_post["shortcode"] = post["shortcode"]
+                        new_post["user_id"] = post["user_id"]
+                        new_post["likes"] = post["liked_by"]
+                        new_post["created_at"] = datetime.datetime.fromtimestamp(post["timestamp"]).strftime("%Y-%m-%d")
+                        self.connection.store_to_collection(new_post, "instagram_new")
+                        count += 1
+                except:
+                    print(1)
+
+        print("--------------------------------")
+        print(f"Number of found: {count}")
--- twitter_data.py
+++ twitter_data.py
@@ -1,79 +1,94 @@
-import time, datetime, json
+import datetime
+import time
+from tweepy import API, OAuthHandler, Cursor, TweepError
+from mongo import MongoHandler
 from preprocessing_functions import preprocess_text
 from secret_keys import consumer_key, consumer_secret, access_token, access_token_secret
-from tweepy import API, OAuthHandler, Cursor, RateLimitError, TweepError
-from pymongo import MongoClient
+
 
 class TweetMiner(object):
 
     api = None
-    # data = []
+    connection = None
 
     def __init__(self):
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         self.api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        self.connection = MongoHandler()
 
     def get_tweets_with_id(self):
 
-        client = MongoClient("mongodb://localhost:27017/")
-        db = client["web_mining"]
-        collection = db["twitter"]
-        x = collection.find({"_id": 1236135735201329153})
+        old_posts = self.connection.retrieve_from_collection("twitter")
+        new_posts = self.connection.retrieve_from_collection("twitter_new")
+        new_ids_list = [row["_id"] for row in new_posts]
         ids_list = [
-            row["_id"] for row in x
-            if not row["full_text"].startswith("RT @") and ("promo" or "giveaway") not in row["full_text"] and len(row["full_text"].split()) >= 5
+            row["_id"] for row in old_posts
+            if not row["_id"] in new_ids_list and not row["full_text"].startswith("RT @") and ("promo" or "giveaway") not in row["full_text"] and len(row["full_text"].split()) >= 5
         ]
-        # print(ids_list)
 
-        count = 0
-
+        print("Starting...")
+        count0 = 0
+        count1 = 0
         for tweet_id in ids_list:
             try:
-                tweet = self.api.get_status(tweet_id, tweet_mode="extended")
-                pre_tweet = self.preprocess_tweet(tweet)
-                # self.data.append(tweet)
-                # json_str = json.dumps(pre_tweet._json)
-                # parsed = json.loads(json_str)
-                print(json.dumps(pre_tweet, indent=4, sort_keys=True))
+                # tweet = self.api.get_status(tweet_id, tweet_mode="extended")._json
+                tweets = self.connection.get_with_id("twitter", {"_id": tweet_id})
+                for tweet in tweets:
+                    _pre_tweet = self.preprocess_tweet(tweet)
+                    # print(json.dumps(pre_tweet, indent=4, sort_keys=True))
+                    count1 += 1
             except TweepError:
-                count += 1
+                count0 += 1
+
+        print("--------------------------------")
+        print(f"Number of found: {count1}")
+        print("--------------------------------")
+        print(f"Number of not found: {count0}")
 
     def preprocess_tweet(self, tweet):
         tweet_dict = dict()
-        tweet_dict["_id"] = tweet._json["id"]
-        tweet_dict["text"] = preprocess_text(tweet._json["full_text"])
-        tweet_dict["hashtags"] = [hashtag["text"] for hashtag in tweet._json["entities"]["hashtags"]]
-        tweet_dict["mentions"] = [hashtag["name"] for hashtag in tweet._json["entities"]["user_mentions"]]
-        tweet_dict["hashtags"] = [hashtag["text"] for hashtag in tweet._json["entities"]["hashtags"]]
-        tweet_dict["urls"] = [hashtag["url"] for hashtag in tweet._json["entities"]["urls"]]
-        tweet_dict["user_id"] = tweet._json["user"]["id"]
-        tweet_dict["user_name"] = tweet._json["user"]["name"]
-        tweet_dict["user_screen_name"] = tweet._json["user"]["screen_name"]
-        tweet_dict["user_location"] = tweet._json["user"]["location"]
-        tweet_dict["user_description"] = tweet._json["user"]["description"]
-        tweet_dict["user_followers"] = tweet._json["user"]["followers_count"]
-        tweet_dict["user_friends"] = tweet._json["user"]["friends_count"]
-        tweet_dict["user_listed"] = tweet._json["user"]["listed_count"]
-        tweet_dict["user_favourites"] = tweet._json["user"]["favourites_count"]
-        ts = time.strftime('%Y-%m', time.strptime(tweet._json["user"]["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
+        tweet_dict["_id"] = tweet["_id"]
+        created_at = time.strftime('%Y-%m-%d', time.strptime(tweet["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
+        tweet_dict["created_at"] = created_at
+        tweet_dict["text"] = preprocess_text(tweet["full_text"])
+        tweet_dict["hashtags"] = [hashtag["text"] for hashtag in tweet["entities"]["hashtags"]]
+        tweet_dict["mentions"] = [hashtag["name"] for hashtag in tweet["entities"]["user_mentions"]]
+        tweet_dict["hashtags"] = [hashtag["text"] for hashtag in tweet["entities"]["hashtags"]]
+        tweet_dict["urls"] = [hashtag["url"] for hashtag in tweet["entities"]["urls"]]
+        tweet_dict["user_id"] = tweet["user"]["id"]
+        tweet_dict["user_name"] = tweet["user"]["name"]
+        tweet_dict["user_screen_name"] = tweet["user"]["screen_name"]
+        tweet_dict["user_location"] = tweet["user"]["location"]
+        tweet_dict["user_followers"] = tweet["user"]["followers_count"]
+        tweet_dict["user_friends"] = tweet["user"]["friends_count"]
+        tweet_dict["user_listed"] = tweet["user"]["listed_count"]
+        tweet_dict["user_favourites"] = tweet["user"]["favourites_count"]
+        ts = time.strftime('%Y-%m', time.strptime(tweet["user"]["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
         date_time_obj = datetime.datetime.strptime(ts, '%Y-%m')
         end_date = datetime.datetime.now()
         num_months = (end_date.year - date_time_obj.year) * 12 + (end_date.month - date_time_obj.month)
         tweet_dict["user_months"] = num_months
-        tweet_dict["user_statuses"] = tweet._json["user"]["statuses_count"]
-        tweet_dict["user_verified"] = int(tweet._json["user"]["verified"])
-        tweet_dict["geo"] = tweet._json["geo"]
-        tweet_dict["place"] = tweet._json["place"]
-        tweet_dict["coordinates"] = tweet._json["coordinates"]
-        tweet_dict["retweets"] = tweet._json["retweet_count"]
-        tweet_dict["favorites"] = tweet._json["favorite_count"]
-        tweet_dict["is_quoted"] = tweet._json["is_quote_status"]
+        tweet_dict["user_statuses"] = tweet["user"]["statuses_count"]
+        tweet_dict["user_verified"] = int(tweet["user"]["verified"])
+        tweet_dict["retweets"] = tweet["retweet_count"]
+        tweet_dict["favorites"] = tweet["favorite_count"]
+        tweet_dict["is_quoted"] = tweet["is_quote_status"]
+        self.connection.store_to_collection(tweet_dict, "twitter_new")
         return tweet_dict
+
+    def get_new_tweets(self):
+        count = 0
+        for tweet in Cursor(self.api.search, q="@#ClimateChange", lang="en", tweet_mode="extended").items():
+            if not tweet._json["full_text"].startswith("RT @") and ("promo" or "giveaway") not in tweet._json["full_text"] and len(tweet._json["full_text"].split()) >= 5:
+                count += 1
+                self.preprocess_tweet(tweet._json)
+        print("--------------------------------")
+        print(f"Number of found: {count}")
 
     # Getting the data
 # i = 0
-# for tweet in Cursor(api.search, q="@mattduss", lang="en", tweet_mode="extended").items(): #q="#ClimateChange"
+# for tweet in Cursor(api.search, q="@", lang="en", tweet_mode="extended").items(): #q="#ClimateChange"
 #     # tweet._json["_id"] = tweet._json.pop("id")
 #     #
 #     # tweet_dict = dict()
