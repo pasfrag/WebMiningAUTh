5c9c5a1b338b76a6a63946ce1579e919cdc4bdcb
tatiana passali <passatati96@gmail.com>
2020-05-24 16:43:35 +0300

    Like Prediction using LSTM neural networks and Pretrained GLOVE Embeddings

--- LikePrediction/NLPbased/like_prediction_model.py
+++ LikePrediction/NLPbased/like_prediction_model.py
@@ -1,40 +1,14 @@
+import pickle
 from sklearn.model_selection import train_test_split
-from LikePrediction.NLPbased.like_prediction_vectorizer import vectorize_train_dataframe, vectorize_test_dataframe
-from mongo import MongoHandler
-import pandas as pd
-import numpy as np
+from LikePrediction.NLPbased.like_prediction_vectorizer import vectorize_train_dataframe, vectorize_test_dataframe,read_tweets_and_instaposts
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn.metrics import accuracy_score, f1_score
-from keras.models import Sequential
-from keras.layers import LSTM, Dropout, Dense, Activation, Embedding, Flatten, Softmax
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-mongo_connect = MongoHandler()
-tweets = mongo_connect.retrieve_from_collection("twitter_final")  # Retrieve tweets from collection
-
-mongo_tweets = pd.DataFrame(list(tweets))
-mongo_tweets = mongo_tweets.sample(frac=1, random_state=1)
-mongo_tweets = mongo_tweets[
-    ['_id', 'text', 'negative', 'positive:', 's_anger', 's_disgust', 's_fear', 's_joy', 's_sadness', 's_surprise',
-     'favorites']]  # Keep emotion analysis features and labels
-mongo_tweets.rename(columns={"positive:": "positive"}, inplace=True)
-# print(mongo_tweets.columns)
-# print(mongo_tweets)
-# Handle like prediction as a classification problem by creating 4 different bins for like prediction (0-1, 2-5, 6-10, 11+)
-labels = mongo_tweets[['favorites']]
-labels['favorites'] = np.where(labels['favorites'].between(0, 1), 0, labels['favorites'])
-labels['favorites'] = np.where(labels['favorites'].between(2, 5), 1, labels['favorites'])
-labels['favorites'] = np.where(labels['favorites'].between(6, 10), 2, labels['favorites'])
-labels['favorites'] = np.where(labels['favorites'] > 10, 3, labels['favorites'])
-
-# print(labels.favorites.value_counts())
+coll_name = 'twitter_final'
+mongo_tweets, labels = read_tweets_and_instaposts(coll_name)
 
 # Split the dataset into train and test
 X_train, X_test, y_train, y_test = train_test_split(mongo_tweets, labels, test_size=0.3, train_size=0.7)
-
-# print(X_train, X_train.shape)
-# print(X_test, X_test.shape)
-# print(y_train, y_train.shape)
-# print(y_test, y_test.shape)
 
 # Vectorize the dataset (tfidf or count vectorizer)
 X_train = vectorize_train_dataframe(X_train)
@@ -58,3 +32,10 @@ y_pred = nb.predict(X_test)
 # Evaluate the model with different metrics
 print(accuracy_score(y_test, y_pred))
 print(f1_score(y_test, y_pred, average='macro'))
+print(precision_score(y_test, y_pred, average="macro"))
+print(recall_score(y_test, y_pred, average="macro"))
+
+# Save model to pickle
+filename = 'models/naive_bayes.pkl'
+with open(filename, 'wb') as file:
+    pickle.dump(nb, file)
